f9b33ff5933b692d275b82c3c3db3452266c0b89
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-04 18:05:09 +0200

    Added restart game keybinding for keyboard

--- VRProjekti/Assets/Scripts/GameManager.cs
+++ VRProjekti/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -74,6 +75,12 @@ public class GameManager : MonoBehaviour
             }
 
             UpdateWarningFillAmount();
+        }
+
+        // Restart game if pressed "Home"
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
+            SceneManager.LoadScene("TowerScene");
         }
     }
 
